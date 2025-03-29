9a3ccbc370c408180909f93a951a68970cceec05
Zhuoxuan-Zhang <zz3052@columbia.edu>
2025-03-29 14:49:26 -0400

    save sample images

--- finetune_horizontal_diffusion.sh
+++ finetune_horizontal_diffusion.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Set dataset path
-DATASET_PATH="/users/zzhan513/data/zzhan513/visual_reasoning/symmetric_training_imgs/horizontal_arc_for_training/train"
+DATASET_PATH="/users/zzhan513/data/zzhan513/visual_reasoning/symmetric_training_imgs/horizontal_arc_for_training/train/"
 PRETRAINED_CHECKPOINT="models/256x256_diffusion.pt"
 
 # Reduce memory usage
 MODEL_FLAGS="--image_size 256 --num_channels 256 --num_res_blocks 2 --num_head_channels 64 --learn_sigma True --use_scale_shift_norm True --attention_resolutions 32,16,8 --class_cond True --resblock_updown True --use_fp16 True"
-DIFFUSION_FLAGS="--diffusion_steps 200 --noise_schedule linear"
+DIFFUSION_FLAGS="--diffusion_steps 1000 --noise_schedule linear"
 TRAIN_FLAGS="--lr 1e-4 --batch_size 2 --use_fp16 True --data_dir $DATASET_PATH --resume_checkpoint $PRETRAINED_CHECKPOINT"
 
 python -m scripts.image_train $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS
--- scripts/classifier_sample.py
+++ scripts/classifier_sample.py
@@ -5,6 +5,7 @@ process towards more realistic images.
 
 import argparse
 import os
+from PIL import Image
 
 import numpy as np
 import torch as th
@@ -27,7 +28,7 @@ def main():
     args = create_argparser().parse_args()
 
     dist_util.setup_dist()
-    logger.configure()
+    logger.configure(dir="samples")
 
     logger.log("creating model and diffusion...")
     model, diffusion = create_model_and_diffusion(
@@ -87,6 +88,13 @@ def main():
         sample = ((sample + 1) * 127.5).clamp(0, 255).to(th.uint8)
         sample = sample.permute(0, 2, 3, 1)
         sample = sample.contiguous()
+        imgs = sample.cpu().numpy()
+        print("imgs.shape", imgs.shape)
+        for i in range(imgs.shape[0]):
+            img = imgs[i]
+            img = Image.fromarray(img)
+            img = img.convert("RGB")
+            img.save(f"samples/sample_{len(all_images) * args.batch_size + i}.png")
 
         gathered_samples = [th.zeros_like(sample) for _ in range(dist.get_world_size())]
         dist.all_gather(gathered_samples, sample)  # gather not supported with NCCL
